f8e4cc54e76f148f5fae12d624f73127dfb71c16
murilorigacci <murilo.r.rocha@aluno.senai.br>
2025-05-16 11:36:32 -0300

    Cria o arquivo modelo-02.js e multiplica dois números

---
+++ modelo-02.js
@@ -0,0 +1,8 @@
+// Faça a multiplicação de dois números
+
+let numero1 = 5;
+let numero2 = 7;
+
+let multiplicacao = numero1 * numero2
+
+console.log(`A multiplicação dos números ${numero1} e ${numero2} é ${multilicacao}`);
